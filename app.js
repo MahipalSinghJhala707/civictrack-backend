46eb67468654b04604322b8c7733fc63d68a97bc
Mahipal Singh Jhala <mahipalsinghjhala@ibms-MacBook-Pro-31.local>
2025-11-15 16:21:45 +0530

    chore: create server.js and remove server start from app.js

--- app.js
+++ app.js
@@ -12,9 +12,3 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
-
-if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
